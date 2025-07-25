41dcea919982176c7fb14ba3ac7d23b3d139ca21
koo-rogie <rn5184@naver.com>
2025-07-25 08:20:59 +0000

    🚀deploy: deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,6 +25,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
+      <p>태스트 문구입니다 </p>
       <Image
         src="/images/model/main-model1.png"
         className="w-full"
